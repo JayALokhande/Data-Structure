74821c1fd28f968808ccd8d6f7f1df32b8b7edce
JayALokhande <jayanillokhande@gmail.com>
2026-08-26 23:21:04 +0530

    Learning Linked List

---
+++ C-Programming/Practice/program524.c
@@ -0,0 +1,30 @@
+//4
+//1+2+3+4 = 10
+
+#include<stdio.h>
+
+int Summation(int iNo)
+{
+    int iSum = 0;
+
+    while(iNo != 0)
+    {
+        iSum = iSum + iNo;
+        iNo--;
+    }
+    return iSum;
+}
+
+
+int main()
+{
+    int iValue = 0,iRet = 0;
+
+    printf("Enter the Frequency :");
+    scanf("%d",&iValue);
+
+    iRet = Summation(iValue);
+    printf("Summation is : %d\n",iRet);
+
+    return 0;
+}
