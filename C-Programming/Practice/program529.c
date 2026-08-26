d8737cf5746e4a6baf4a063ced88585b6da826de
JayALokhande <jayanillokhande@gmail.com>
2026-08-26 23:26:31 +0530

    Learning Linked List

---
+++ C-Programming/Practice/program529.c
@@ -0,0 +1,33 @@
+//4
+//4*3*2*1 = 24
+
+#include<stdio.h>
+
+typedef unsigned long int ULONG;
+
+ULONG Factorial(int iNo)
+{
+    ULONG iFact = 1;
+
+    while(iNo > 0)
+    {
+        iFact = iFact * iNo;
+        iNo--;
+    }
+    return iFact;
+}
+
+
+int main()
+{
+    int iValue = 0;
+    ULONG iRet = 0;
+
+    printf("Enter the Frequency :");
+    scanf("%lu",&iValue);
+
+    iRet = Factorial(iValue);
+    printf("Factorial is : %d\n",iRet);
+
+    return 0;
+}
